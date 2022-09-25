1f9c20a646d22c0abd886057308822e5630da628
luisccalves <luisccalves@hotmail.com>
2022-09-25 17:57:24 +0100

    Update prophet.py

--- prophet.py
+++ prophet.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 
-#instalar o yfinance
-pip install yfinance
+# instalar o yfinance
+# pip install yfinance
+
+import warnings
+from datetime import datetime, timedelta
 
 #import bibliotecas
 import pandas as pd
-import yfinance as yf
-from datetime import datetime
-from datetime import timedelta
 import plotly.graph_objects as go
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly, plot_components_plotly
-import warnings
+import yfinance as yf
+
+from prophet import Prophet
+from prophet.plot import plot_components_plotly, plot_plotly
+
 warnings.filterwarnings('ignore')
 pd.options.display.float_format = '${:,.2f}'.format
 
@@ -22,31 +24,31 @@ df_eth.tail()
 
 df_eth.reset_index(inplace=True)
 
-df_eth
+print(df_eth)
 
 df = df_eth[["Date", "Adj Close"]]
-df.rename(columns = {'Date': 'ds', 'Adj Close': 'y' }, inplace=True)
+df.rename(columns={'Date': 'ds', 'Adj Close': 'y'}, inplace=True)
 
-df
+print(df)
 
 # Grafico Preço de fechamento
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=df['ds'], y = df['y']))
+fig.add_trace(go.Scatter(x=df['ds'], y=df['y']))
 
-model = Prophet( seasonality_mode='multiplicative')
+model = Prophet(seasonality_mode='multiplicative')
 model.fit(df)
 
-#criar df com datas no futuro
+# criar df com datas no futuro
 df_futuro = model.make_future_dataframe(periods=60)
-df_futuro.tail (60)
+df_futuro.tail(60)
 
-#previsao
+# previsao
 previsao = model.predict(df_futuro)
-previsao
+print(previsao)
 
 previsao[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(60)
 
-#grafico
+# grafico
 plot_plotly(model, previsao)
 
 plot_components_plotly(model, previsao)
